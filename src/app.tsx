6b5647b854892ebcef4e7173030f68f073f3795f
44r0n <aaron.sanchez.1988@icloud.com>
2022-12-07 13:08:16 +0100

    Añadiendo tailwind y corrigiendo package.json

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 
 function render() {
 	const root = createRoot(document.getElementById('app'));
-	root.render(<h2>Hello from React!</h2>);
+	root.render(<h2 className='text-3xl font-bold underline'>Hello from React!</h2>);
 }
 
 render();
